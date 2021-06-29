151671d830c257ef57947169092e3f2cf78d2b37
Daniel Cotillas Ruiz <danicotillas@gmail.com>
2021-06-29 11:52:59 +0200

    Delete themes directory

--- themes/generatepress-child/functions.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-add_action( 'wp_enqueue_scripts', function() {
-    wp_dequeue_style( 'generate-fonts' );
-} );
-
-add_action( 'admin_init', function() {
-    add_filter( 'generate_google_fonts_array', '__return_empty_array' );
-} );
